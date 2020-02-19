bb4a8a273c31d28605f7458ea51ae5655d0dcaba
battleplan <57051105+battleplan@users.noreply.github.com>
2020-02-19 18:41:06 +0000

    list stuff

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,57 @@
+import { add } from '../spec/utils';
 
-import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+const addButton = document.getElementById('addButton') as HTMLInputElement;
+const itemToAdd = document.getElementById('itemToAdd') as HTMLInputElement;
+const list = document.getElementById('list') as HTMLUListElement;
+const count = document.getElementById('count') as HTMLSpanElement;
+
+let items: ShoppingItem[] = [];
+
+const storedItems = localStorage.getItem('shopping-list');
+
+if (storedItems) {
+    items = JSON.parse(storedItems);
+    items.forEach(createItemInDom);
+    updateCount();
+}
+
+addButton.addEventListener('click', addTheItem);
+
+itemToAdd.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+        addTheItem();
+    }
+});
+
+interface ShoppingItem {
+    description: string;
+}
+
+function addTheItem() {
+    const item = itemToAdd.value;
+    const thingToAdd: ShoppingItem = { description: item };
+    items = [...items, thingToAdd];
+    createItemInDom(thingToAdd);
+    itemToAdd.value = ''; //  clear it out
+    itemToAdd.focus(); //  puts the cursor there ready for the next item
+    saveIt();
+    updateCount();
+}
+
+function createItemInDom(item: ShoppingItem) {
+    const li = document.createElement('li') as HTMLLIElement;
+    li.classList.add('list-group-item');
+    const text = document.createTextNode(item.description);
+    li.appendChild(text);
+    list.appendChild(li);
+}
+// <li class="list-group-item">Cras justo odio</li>
+
+function saveIt() {
+    localStorage.setItem('shopping-list', JSON.stringify(items));
+}
+
+function updateCount() {
+    count.innerText = items.length.toString();
+}
 
